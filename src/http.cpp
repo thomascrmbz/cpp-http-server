d0b9117dec79358123a40e795c2d472c020256b3
TrikThom <thomas@trikthom.com>
2020-11-11 10:24:50 +0100

    Fixed concurrent connection throw segmentation fault

--- src/http.cpp
+++ src/http.cpp
@@ -39,35 +39,44 @@ void Server::listen(int port) const {
     perror("bind failed");
     exit(0);
   }
+
   if (::listen(socket, 3) < 0) {
     perror("listen");
     exit(0);
   }
 
   while (true) {
-    int new_socket;
-    struct sockaddr_in addr;
-    int addrlen = sizeof(addr);
-    char buffer[1024] = {0};
+    int new_socket = 0;
+    struct sockaddr_in client_addr;
+    int addrlen = sizeof(client_addr);
 
-    if ((new_socket = accept(socket, (struct sockaddr *) &addr, (socklen_t*) &addrlen)) <0) {
+    if ((new_socket = accept(socket, (struct sockaddr *) &client_addr, (socklen_t*) &addrlen)) < 0) {
       perror("accept");
       exit(0);
     }
 
-    read(new_socket, buffer, 1024);
-    Request request(buffer);
+    auto connection_thread = [](int socket) {
+      while (true) {
+        char buffer[1024];
+        bzero(buffer, 1024);
 
+        if (read(socket, buffer, 1024) <= 0) {
+          close(socket);
+          break;
+        }
 
-    auto f = [](int socket) {
-      std::string text = "HTTP/1.1 200 OK\n\nHello World!";
-      send(socket, text.c_str(), text.length(), 0);
-      printf("Response message sent\n\n");
-      close(socket);
+        Request request(buffer);
+        
+        std::cout << "path: " << request.get_path() << std::endl;
+
+        std::string text = "HTTP/1.1 200 OK\n\nHello World!";
+        send(socket, text.c_str(), text.length(), 0);
+        printf("response message sent\n\n");
+        close(socket);
+      }
     };
 
-    std::thread th(f, new_socket);
-    th.detach();
-    
+    std::thread th(connection_thread, new_socket);
+    th.join();
   }
 }
